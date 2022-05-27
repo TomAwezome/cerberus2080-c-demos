f545f8a2d5e7f7266591f7b0889ce882bf998943
TomAwezome <sonidd1@yahoo.com>
2022-05-27 05:20:56 -0400

    Optimize 80x60 bitmap engine.

--- bitmap-engine80x60/main.c
+++ bitmap-engine80x60/main.c
@@ -7,143 +7,164 @@
 
 const U8 bitmap_area[VIDEO_RAM_SIZE];
 
-U0 ScreenBitmapClear()
-{
-	U16 i;
-	for (i = 0; i < VIDEO_RAM_SIZE; i++)
-		bitmap_area[i] = 0;
-}
-
 U0 ScreenBitmapDraw(U16 x, U16 y)
 {
 	U16 cell_x = x / 2;
 	U16 cell_y = y / 2;
-	U16 quad_x = x % 2;
-	U16 quad_y = y % 2;
-	U8 byte = bitmap_area[40 * cell_y + cell_x];
+	U16 quad_x = x & 1;
+	U16 quad_y = y & 1;
+	U16 index = 40 * cell_y + cell_x;
+	U8 byte = bitmap_area[index];
 
 	switch (quad_x)
 	{
 		case 0:
 			switch (quad_y)
 			{
-				case 0:
-					bitmap_area[40 * cell_y + cell_x] = byte | 1;
+				case 0: // QUAD TOP LEFT
+					switch (byte)
+					{
+						case 0x20: // empty char
+							bitmap_area[index] = 0x01; // set top left char
+							break;
+						case 0x02: // top right char
+							bitmap_area[index] = 0x03; // set top char
+							break;
+						case 0x87: // bottom right char
+							bitmap_area[index] = 0x86; // set top left bottom right char
+							break;
+						case 0x04: // bottom left char
+							bitmap_area[index] = 0x05; // set left char
+							break;
+						case 0x83: // bottom char
+							bitmap_area[index] = 0x82; // set all but top right char
+							break;
+						case 0x85: // right char
+							bitmap_area[index] = 0x84; // set all but bottom left char
+							break;
+						case 0x06: // top right bottom left char
+							bitmap_area[index] = 0x07; // set all but bottom right char
+							break;
+						case 0x81: // all but top left char
+							bitmap_area[index] = 0xA0; // set inverse empty char
+							break;
+					}
 					break;
-				case 1:
-					bitmap_area[40 * cell_y + cell_x] = byte | 4;
+				case 1: // QUAD BOTTOM LEFT
+					switch (byte)
+					{
+						case 0x20: // empty char
+							bitmap_area[index] = 0x04; // set bottom left char
+							break;
+						case 0x02: // top right char
+							bitmap_area[index] = 0x06; // set top right bottom left char
+							break;
+						case 0x87: // bottom right char
+							bitmap_area[index] = 0x83; // set bottom char
+							break;
+						case 0x01: // top left char
+							bitmap_area[index] = 0x05; // set left char
+							break;
+						case 0x03: // top char
+							bitmap_area[index] = 0x07; // set all but bottom right char
+							break;
+						case 0x85: // right char
+							bitmap_area[index] = 0x81; // set all but top left
+							break;
+						case 0x86: // bottom right top left char
+							bitmap_area[index] = 0x82; // set all but top right char
+							break;
+						case 0x84: // all but bottom left char
+							bitmap_area[index] = 0xA0; // set inverse empty char
+							break;
+					}
 					break;
 			}
 			break;
 		case 1:
 			switch (quad_y)
 			{
-				case 0:
-					bitmap_area[40 * cell_y + cell_x] = byte | 2;
+				case 0: // QUAD TOP RIGHT
+					switch (byte)
+					{
+						case 0x20: // empty char
+							bitmap_area[index] = 0x02; // set top right char
+							break;
+						case 0x01: // top left char
+							bitmap_area[index] = 0x03; // set top char
+							break;
+						case 0x87: // bottom right char
+							bitmap_area[index] = 0x85; // set right char
+							break;
+						case 0x04: // bottom left char
+							bitmap_area[index] = 0x06; // set top right bottom left char
+							break;
+						case 0x83: // bottom char
+							bitmap_area[index] = 0x81; // set all but top left char
+							break;
+						case 0x05: // left char
+							bitmap_area[index] = 0x07; // set all but bottom right char
+							break;
+						case 0x86: // bottom right top left char
+							bitmap_area[index] = 0x84; // set all but bottom left char
+							break;
+						case 0x82: // all but top right char
+							bitmap_area[index] = 0xA0; // set inverse empty char
+							break;
+					}
 					break;
-				case 1:
-					bitmap_area[40 * cell_y + cell_x] = byte | 8;
+				case 1: // QUAD BOTTOM RIGHT
+					switch (byte)
+					{
+						case 0x20: // empty char
+							bitmap_area[index] = 0x87; // set bottom right char
+							break;
+						case 0x02: // top right char
+							bitmap_area[index] = 0x85; // set right char
+							break;
+						case 0x04: // bottom left char
+							bitmap_area[index] = 0x83; // set bottom char
+							break;
+						case 0x01: // top left char
+							bitmap_area[index] = 0x86; // set bottom right top left char
+							break;
+						case 0x03: // top char
+							bitmap_area[index] = 0x84; // set all but bottom left char
+							break;
+						case 0x05: // left char
+							bitmap_area[index] = 0x82; // set all but top right char
+							break;
+						case 0x06: // top right bottom left char
+							bitmap_area[index] = 0x81; // set all but top left char
+							break;
+						case 0x07: // all but bottom right char
+							bitmap_area[index] = 0xA0; // set inverse empty char
+							break;
+					}
 					break;
 			}
 			break;
 	}
 }
-// 0000b empty
-// 0001b top left
-// 0010b top right
-// 0100b bottom left
-// 1000b bottom right
-// 0011b top
-// 1100b bottom
-// 0101b left
-// 1010b right
-// 1001b top left bottom right
-// 0110b bottom left top right
-// 1110b all but top left
-// 1101b all but top right
-// 1011b all but bottom left
-// 0111b all but bottom right
-// 1111b is all
-U0 ScreenBitmapRender()
-{
-	U16 i;
-	U8 byte;
-	
-	for (i = 0; i < VIDEO_RAM_SIZE; i++)
-	{
-		byte = bitmap_area[i];
-		switch (byte)
-		{
-			case 0:
-				video_ram[i] = 0x20; // space char
-				break;
-			case 1:
-				video_ram[i] = 0x01; // top left char
-				break;
-			case 2:
-				video_ram[i] = 0x02; // top right char
-				break;
-			case 4:
-				video_ram[i] = 0x04; // bottom left char
-				break;
-			case 8:
-				video_ram[i] = 0x87; // bottom right char
-				break;
-			case 3:
-				video_ram[i] = 0x03; // top char
-				break;
-			case 12:
-				video_ram[i] = 0x83; // bottom char
-				break;
-			case 5:
-				video_ram[i] = 0x05;// left char
-				break;
-			case 10:
-				video_ram[i] = 0x85;// right char
-				break;
-			case 9:
-				video_ram[i] = 0x86;// top left bottom right char
-				break;
-			case 6:
-				video_ram[i] = 0x06; // bottom left top right char
-				break;
-			case 14:
-				video_ram[i] = 0x81; // all but top left char
-				break;
-			case 13:
-				video_ram[i] = 0x82; // all but top right char
-				break;
-			case 11:
-				video_ram[i] = 0x84; // all but bottom left char
-				break;
-			case 7:
-				video_ram[i] = 0x07; // all but bottom right char
-				break;
-			case 15:
-				video_ram[i] = 0xA0; // all char
-				break;
-		}
-	}
-
-}
 
 int main()
 {
 	U16 x = 0;
-	U16 y = 7;
-	ScreenBitmapClear();
+	U16 y = 0;
+	memset(bitmap_area, 0x20, VIDEO_RAM_SIZE);
 	while (TRUE)
 	{
+		memset(bitmap_area, 0x20, VIDEO_RAM_SIZE);
 		ScreenBitmapDraw(x, y);
-		if (x & 1)
-			ScreenBitmapDraw(x, y + 1);
-		ScreenBitmapRender();
+		memcpy(VIDEO_RAM, bitmap_area, VIDEO_RAM_SIZE);
 		x++;
 		if (x >= 80)
 		{
 			x = 0;
 			y++;
 		}
+		if (y >= 60)
+			y = 0;
 	}
 
 	return 0;
